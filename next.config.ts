97105ab353f1bf9556672c57d08c6630dcee2378
SR <>
2025-09-17 23:49:40 +0530

    config

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,12 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
     domains: [
-      'localhost',
-      'ceullkuvmxvkhwpjayfp.supabase.co',
-      'lh3.googleusercontent.com',
+      "localhost",
+      "ceullkuvmxvkhwpjayfp.supabase.co",
+      "lh3.googleusercontent.com",
     ],
   },
+  output: "standalone" // ✅ good for Vercel deploys
 };
 
 export default nextConfig;
