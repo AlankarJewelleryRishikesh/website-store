2f9e99d3a5fbcb03900b5901dfa42f7de421656a
SR <>
2025-09-17 22:51:14 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
-};
-// next.config.js
-module.exports = {
   images: {
-    domains: ['localhost', 'ceullkuvmxvkhwpjayfp.supabase.co','lh3.googleusercontent.com'],
+    domains: [
+      'localhost',
+      'ceullkuvmxvkhwpjayfp.supabase.co',
+      'lh3.googleusercontent.com',
+    ],
   },
 };
 
